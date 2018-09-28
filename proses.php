105dcfde0c1e1665cefb8f5374f74eb9f1403232
Fajar Febbi Aldianto <43521209+fajarfaldianto18@users.noreply.github.com>
2018-09-28 10:11:01 +0700

    proses.php
    form hobi

--- proses.php
+++ proses.php
@@ -1,4 +1,26 @@
 <?php
+		
+		if(isset($_POST['kirim'])) {
+			$username=$_POST=['username']
+			$password=$_POST=['password']
+			 $user=array(
+			 	"user"=>"yoga",
+			 	"pass"=>"maulana"):
 
+			 if ($username==$user['user']&&$password==$user['pass']){
+			 	echo "Berhasil SELALU:"."<br>>";
+
+			 	?>
+
+			 	<form method="POST">
+				 		 mancing<input type="checkbox" name="hobi"[]" value="renang"><br><br>
+					mancing<input type="checkbox" name="hobi"[]" value="badminton"><br><br>
+					mancing<input type="checkbox" name="hobi"[]" value="sepak bola"><br><br>
+					mancing<input type="checkbox" name="hobi"[]" value="mancing"><br><br>
+					choose photo<input type="file" name="foto"><br>
+			 	</form>
+
+
+			 	<input type="submit" name="kirim" value="input"> 
 
 ?>
